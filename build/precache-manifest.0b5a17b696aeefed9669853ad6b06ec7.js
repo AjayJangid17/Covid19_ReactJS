503aabcff7f98358554c01e14e4e59b52b631004
ajayjangid17 <ajayjangid178@gmail.com>
2021-07-11 00:17:45 +0530

    build file

---
+++ build/precache-manifest.0b5a17b696aeefed9669853ad6b06ec7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62517745cf6286694c0f8944a95afcbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac7f90ab27769b737711",
+    "url": "/static/css/main.8cefe2a2.chunk.css"
+  },
+  {
+    "revision": "5e202acd216feb92eb81",
+    "url": "/static/js/2.6bfd3988.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/static/js/2.6bfd3988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac7f90ab27769b737711",
+    "url": "/static/js/main.c9f4a4d2.chunk.js"
+  },
+  {
+    "revision": "a88bf4d6dd92ba8887a4",
+    "url": "/static/js/runtime-main.2ed402ee.js"
+  },
+  {
+    "revision": "6001db859ecea5110f2167bc19f5588b",
+    "url": "/static/media/covid-19.6001db85.gif"
+  },
+  {
+    "revision": "3b7d3730374bdbf1fc10b4685450a488",
+    "url": "/static/media/image.3b7d3730.png"
+  }
+]);
